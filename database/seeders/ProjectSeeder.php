6465392d41ca540074a8a0eb14c85d5cf1f32cbf
aryadeifiordi <arya.93.ar@gmail.com>
2024-05-22 16:14:36 +0200

    seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -18,6 +18,7 @@ class ProjectSeeder extends Seeder
         Project::create([
             'name' => 'Nome del tuo progetto',
             'description' => 'Descrizione del tuo progetto',
+            'title' => 'titolo'
         ]);
 
         
