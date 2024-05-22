8f38b1aa5f156e8fe5358876706a5dc140f88d98
aryadeifiordi <arya.93.ar@gmail.com>
2024-05-22 21:33:12 +0200

    CRUD on ProjectController

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,18 @@ use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -20,5 +23,6 @@ Route::middleware('auth')->group(function () {
     
     Route::resource('admin/projects', ProjectController::class);
 });
+
 
 require __DIR__.'/auth.php';
